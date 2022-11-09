185f9e04a51bb924e90dcb3afe0f7c1dfd82559e
sumin <ski03257@naver.com>
2022-11-10 05:54:56 +0900

    Add third color

--- src/layouts/notifications/index.js
+++ src/layouts/notifications/index.js
@@ -30,6 +30,15 @@ import pants23 from "../../assets/images/pants23.png";
 import pants24 from "../../assets/images/pants24.png";
 import pants25 from "../../assets/images/pants25.png";
 import pants26 from "../../assets/images/pants26.png";
+import person3s from "../../assets/images/person3s.png";
+import pants31 from "../../assets/images/pants31.png";
+import pants32 from "../../assets/images/pants32.png";
+import pants33 from "../../assets/images/pants33.png";
+import pants34 from "../../assets/images/pants34.png";
+import pants35 from "../../assets/images/pants35.png";
+import pants36 from "../../assets/images/pants36.png";
+import nextArrow from "../../assets/images/nextArrow.png";
+import preArrow from "../../assets/images/preArrow.png";
 
 function Notifications() {
   const settings = {
@@ -249,6 +258,109 @@ function Notifications() {
           </MDBox>
         </MDBox>
         {/** 2. 끝 */}
+        {/** 3. 시작 */}
+        <MDBox mb={30}>
+          <Grid container spacing={4}>
+            <div className="topoftop" />
+            <Grid container xs={7}>
+              <span className="imgspan" />
+              <img src={person3s} alt="person" />
+            </Grid>
+            <Grid container xs={5}>
+              <Grid container xs={12}>
+                <div className="topoftop2" />
+                <div className="topColor1" />
+                <div>
+                  <span className="firstspan">Top</span>
+                  <span className="secondspan">Black Navy</span>
+                  <span>#322B2D</span>
+                </div>
+              </Grid>
+
+              <Grid container xs={12}>
+                <div className="bottomColor3" />
+                <div>
+                  <span className="firstspan">Bottom</span>
+                  <span className="secondspan">Black</span>
+                  <span>#0D0B0B</span>
+                </div>
+              </Grid>
+            </Grid>
+          </Grid>
+
+          <MDBox mt={40} ml={10} mr={10}>
+            <h1>Recommend Items</h1>
+            <Grid container spacing={15}>
+              <Grid item xs={4}>
+                <MDBox mt={10} mb={10}>
+                  <Card>
+                    <MDBox mt={1} mb={1} mr={0.5} ml={0.5}>
+                      <img src={pants31} alt="person" />
+                      <span className="brandspan">Brand | 모드나인</span>
+                      <span className="namespan">Name | black stardust -MOD1w</span>
+                      <span className="pricespan">Price | ₩95,400</span>
+                    </MDBox>
+                  </Card>
+                </MDBox>
+                <MDBox mt={10} mb={3}>
+                  <Card>
+                    <MDBox mt={1} mb={1} mr={0.5} ml={0.5}>
+                      <img width={386} src={pants34} alt="person" />
+                      <span className="brandspan">Brand | 어반드레스</span>
+                      <span className="namespan">Name | buckle wide denim panats</span>
+                      <span className="pricespan">Price | ₩37,900</span>
+                    </MDBox>
+                  </Card>
+                </MDBox>
+              </Grid>
+              <Grid item xs={4}>
+                <MDBox mt={10} mb={10}>
+                  <Card>
+                    <MDBox mt={1} mb={1} mr={0.5} ml={0.5}>
+                      <img src={pants32} alt="person" />
+                      <span className="brandspan">Brand | 가까이 유니언즈</span>
+                      <span className="namespan">Name | 원턱 와이드 팬츠</span>
+                      <span className="pricespan">Price | ₩36,400</span>
+                    </MDBox>
+                  </Card>
+                </MDBox>
+                <MDBox mt={10} mb={3}>
+                  <Card>
+                    <MDBox mt={1} mb={1} mr={0.5} ml={0.5}>
+                      <img src={pants35} alt="person" />
+                      <span className="brandspan">Brand | 포트너스</span>
+                      <span className="namespan">Name | 투턱 와이드 진</span>
+                      <span className="pricespan">Price | ₩109,400</span>
+                    </MDBox>
+                  </Card>
+                </MDBox>
+              </Grid>
+              <Grid item xs={4}>
+                <MDBox mt={10} mb={10}>
+                  <Card>
+                    <MDBox mt={1} mb={1} mr={0.5} ml={0.5}>
+                      <img src={pants33} alt="person" />
+                      <span className="brandspan">Brand | 무신사 스탠다드</span>
+                      <span className="namespan">Name | 와이드 데님 팬츠</span>
+                      <span className="pricespan">Price | ₩47,000</span>
+                    </MDBox>
+                  </Card>
+                </MDBox>
+                <MDBox mt={10} mb={3}>
+                  <Card>
+                    <MDBox mt={1} mb={1} mr={0.5} ml={0.5}>
+                      <img src={pants36} alt="person" />
+                      <span className="brandspan">Brand | 굿라이프웍스</span>
+                      <span className="namespan">Name | 와이드 빅포켓 카고 팬츠</span>
+                      <span className="pricespan">Price | ₩46,400</span>
+                    </MDBox>
+                  </Card>
+                </MDBox>
+              </Grid>
+            </Grid>
+          </MDBox>
+        </MDBox>
+        {/** 3. 끝 */}
       </Slider>
       <Footer />
     </DashboardLayout>
